06a249da7eed76f48b9e44a0f4af0ba15b5f3be1
Dayeya <129618322+dayeya@users.noreply.github.com>
2023-11-15 11:55:08 +0200

    Fixed spaces

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -35,33 +35,21 @@ fn save_file_inside_db(app_db: State<'_, Table>, file: &str) -> Option<String> {
         Ok(()) => Ok(()),
         _ => panic!("Problem!")
     };
-
-    // Checks path
     fs::create_dir_all(IMG_FOLDER_PATH).ok();
 
-    // No type hinting, type is &OsStr
+    // type is &OsStr
     let file_name = file_name(file);
-
     let combined_path = Path::new(IMG_FOLDER_PATH).join(file_name);
-    
-    // Copy the image to the assets folder.
     fs::copy(file, &combined_path).ok();
-
-
-    // Send to frontend final path.
     let final_path: String = IMG_FOLDER_PATH.to_owned() + file_name.to_str().unwrap();
     Some(final_path)
 }
 
 #[tauri::command] 
 fn compress(app_db: State<'_, Table>) -> Option<String> {
-
     let files: Result<Vec<String>, Error> = app_db.fetch_all_files();
-
     if let Ok(files) = files { 
         let mut pack: Package = Package::with_files(files);
-
-        // compress all uploaded files.
         pack.compress_all();
         Some("Success".to_string())
     }
@@ -71,12 +59,10 @@ fn compress(app_db: State<'_, Table>) -> Option<String> {
 }
 
 fn main() -> Result<(), Error> {
-
     env::set_var("RUST_BACKTRACE", "1");
     let app_db: Table;
-
-    // Different scope, for 'temp_table_name' to be dropped
     {
+        // Different scope, for 'temp_table_name' to be dropped
         let temp_table_name: String = String::from("files");
         app_db = Table {
             table_name: temp_table_name.clone(), 
@@ -85,24 +71,21 @@ fn main() -> Result<(), Error> {
             )", temp_table_name.clone()),
         }
     }
-
-    // Creating DB for 'app' then handling errors.
+    // Creating DB for 'app' and then handling errors.
     let _res_of_db: Result<(), Error> = match app_db.create_table() { 
         Ok(()) => Ok(()),
-        _ => panic!("Problem as opening table!")
+        _ => panic!("Opening table has raised an error!")
     }; 
-
     let _res_of_dir: Result<(), std::io::Error> = match create_img_folder() {
         Ok(()) => Ok(()),
-        _ => panic!("Problem at opening img folder!")
+        _ => panic!("Opening Image folder has raised an error!")
     };
 
-
+    // Boot the application.
     tauri::Builder::default()
     .manage(app_db)
     .invoke_handler(tauri::generate_handler![save_file_inside_db, compress])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
-
     Ok(())
 }
